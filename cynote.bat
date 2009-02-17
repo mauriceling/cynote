30ed13d0c45a83a5471dba8b3192fbae147567d9
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2009-02-17 11:28:06 +0000

    bootstrap to use easy_install to manage dependencies
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@3 796f4f98-6b54-0410-9c67-d7c569393209

--- cynote.bat
+++ cynote.bat
@@ -1,9 +1,5 @@
+python ez_setup.py
 
-if exist web2py.exe goto web2pyexe
-if exist web2py.py goto web2py
+python easy_install "biopython==1.49"
 
-:web2pyexe
-web2py.exe --config=options.py
-
-:web2py
-python web2py.py --ip=127.0.0.1 --port=8000 --password=admin
+python web2py.py --ip=localhost --port=8000 --password=admin
