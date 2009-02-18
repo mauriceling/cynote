2e9a10d483f0e4469695727cabfd209ca4b63ccf
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2009-02-18 06:33:33 +0000

    adds python path if not there
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@7 796f4f98-6b54-0410-9c67-d7c569393209

--- cynote.bat
+++ cynote.bat
@@ -1,5 +1,17 @@
+set python_directory=c:\Python25
+
 python ez_setup.py
+if errorlevel 1 goto setpath
+if errorlevel 0 goto check_python_modules
 
+:setpath
+set PATH=%PATH%;%python_directory%;%python_directory%/Scripts
+python ez_setup.py
+goto check_python_modules
+
+:check_python_modules
 easy_install "biopython==1.49"
+goto run_web2py
 
+:run_web2py
 python web2py.py --ip=localhost --port=8000 --password=admin
