62e2938e3515e9bd1e92fc5592c21fc2bec38388
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2009-06-19 01:56:20 +0000

    compliant to PEP 8
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@48 796f4f98-6b54-0410-9c67-d7c569393209

--- controllers/replicate.py
+++ controllers/replicate.py
@@ -4,45 +4,63 @@ import StringIO
         
 def export(): 
     # export the entire database
-    if session.username == None: redirect(URL(r=request,f='../account/log_in'))
-    form_user=FORM(TR("",INPUT(_type="submit",_name="ex_user",_value="Export User List")))
-    if form_user.accepts(request.vars): redirect(URL(r=request,f='export_user_list')) 
-    form_cynote=FORM(TR("",INPUT(_type="submit",_name="ex_cynote",_value="Export Cynote Database"))) 
-    if form_cynote.accepts(request.vars): redirect(URL(r=request,f='export_cynote_database')) 
+    if session.username == None:
+        redirect(URL(r=request, f='../account/log_in'))
+    form_user = FORM(
+            TR("",
+               INPUT(_type="submit", _name="ex_user",
+                     _value="Export User List")))
+    if form_user.accepts(request.vars):
+        redirect(URL(r=request, f='export_user_list')) 
+    form_cynote = FORM(
+            TR("",
+               INPUT(_type="submit", _name="ex_cynote",
+                     _value="Export Cynote Database"))) 
+    if form_cynote.accepts(request.vars):
+        redirect(URL(r=request, f='export_cynote_database')) 
     return dict(form_cynote=form_cynote,
                 form_user=form_user) 
 
 def export_cynote_database():
-    s=StringIO.StringIO() 
+    s = StringIO.StringIO() 
     cynotedb.export_to_csv_file(s) 
-    response.headers['Content-Type']='text/csv' 
+    response.headers['Content-Type'] = 'text/csv' 
     return s.getvalue() 
 
 def export_user_list():
     s=StringIO.StringIO() 
     userdb.export_to_csv_file(s) 
-    response.headers['Content-Type']='text/csv' 
+    response.headers['Content-Type'] = 'text/csv' 
     return s.getvalue() 
         
 def import_sync(): 
-    form=FORM(INPUT(_type='file',_name='data'),INPUT (_type='submit')) 
+    form = FORM(
+            INPUT(_type='file', _name='data'),
+            INPUT(_type='submit')) 
     if form.accepts(request.vars):      
         cynotedb.import_from_csv_file(form.vars.data.file) 
            # for every table 
         for table in cynotedb.tables: 
                  # for every uuid, delete all but the most 
-            items = cynotedb(cynotedb[table].id>0).select(cynotedb[table].id,
-            cynotedb[table].uuid,orderby=~cynotedb[table].modified_on,
-            groupby=cynotedb[table].uuid)                  
+            items = cynotedb(cynotedb[table].id>0) \
+                    .select(cynotedb[table].id,
+                            cynotedb[table].uuid,orderby = ~cynotedb[table] \
+                            .modified_on,
+                            groupby = cynotedb[table].uuid)                  
             for item in items: 
-                cynotedb((cynotedb[table].uuid==item.uuid)&(cynotedb[table].id!= item.id)).delete    
+                cynotedb((cynotedb[table].uuid == item.uuid) & \
+                         (cynotedb[table].id != item.id)).delete    
     return dict(form=form)
     
 def replication():        
-    form = FORM(TABLE(TR("", INPUT(_type="submit",_value="SUBMIT"))))
+    form = FORM(
+            TABLE(
+                TR("", INPUT(_type="submit", _value="SUBMIT"))))
     if form.accepts(request.vars,session):
-        rows=cynotecynotedb((cynotecynotedb.entry.notebook==cynotecynotedb.entry.id)&\
-            (cynotecynotedb.comment.entry_id==cynotecynotedb.entry.id)).select()
+        rows = cynotecynotedb((cynotecynotedb.entry.notebook == \
+                               cynotecynotedb.entry.id) & \
+                              (cynotecynotedb.comment.entry_id == \
+                               cynotecynotedb.entry.id)).select()
         for row in rows:
             print row.entry.id, row.notebook.id, row.comment.id 
         #rows=cynotecynotedb().select(
@@ -50,14 +68,18 @@ def replication():
            # cynotecynotedb.comment.ALL,
             #left=[cynotecynotedb.comment.on(cynotecynotedb.entry.notebook_id==cynotecynotedb.notebook.id),
              # cynotecynotedb.comment.on(cynotecynotedb.comment.entry_id==cynotecynotedb.entry.id)])   
-    return dict(form=form,rows=rows)
+    return dict(form=form,
+                rows=rows)
 
 
 def zip(): 
-    form = FORM(TABLE("", INPUT(_type="submit",_value="SUBMIT")))
+    form = FORM(TABLE("", INPUT(_type="submit", _value="SUBMIT")))
     if form.accepts(request.vars):
-        uploaded_files = os.listdir([os.getcwd(),'applications',request.application, 'uploads'])    
-        zipf = zipfile.Zipfile(os.getcwd(), 'applications',request.application, 'uploads','uploads_backup.zip','w')                           
+        uploaded_files = os.listdir([os.getcwd(),'applications',
+                                     request.application, 'uploads'])    
+        zipf = zipfile.Zipfile(os.getcwd(), 'applications',
+                               request.application,
+                               'uploads','uploads_backup.zip', 'w')                           
         for f in uploaded_files: 
                 try: zipf.write(f) 
                 except: pass            
