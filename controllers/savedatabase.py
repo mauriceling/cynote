fbd2586f9991f0dc94cc5adbea09f5e63693b645
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2009-06-19 01:54:09 +0000

    compliant to PEP 8
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@47 796f4f98-6b54-0410-9c67-d7c569393209

--- controllers/savedatabase.py
+++ controllers/savedatabase.py
@@ -2,26 +2,28 @@ import os
 import datetime
 from ftplib import FTP
 
-now=datetime.datetime.utcnow()
+now = datetime.datetime.utcnow()
 
 def form():
-    if session.username == None: redirect(URL(r=request,f='../account/log_in'))    
+    if session.username == None:
+        redirect(URL(r=request, f='../account/log_in'))    
     form = FORM(TABLE(TR("FTP Server: ", 
-                        INPUT(_type="text",_name="ftpserver")),
+                        INPUT(_type="text", _name="ftpserver")),
                       TR("FTP Username: ", 
-                        INPUT(_type="text",_name="ftpusername")),
+                        INPUT(_type="text", _name="ftpusername")),
                       TR("FTP Password: ", 
-                        INPUT(_type="password",_name="ftppassword")),
-                      TR("", INPUT(_type="submit",_value="SUBMIT"))))
+                        INPUT(_type="password", _name="ftppassword")),
+                      TR("",
+                         INPUT(_type="submit", _value="SUBMIT"))))
     if form.accepts(request.vars,session): 
         table_list = []
-        table_backup=[]
+        table_backup = []
         backup_path = str(os.sep).join([os.getcwd(), 'applications', 
                         request.application, 'databases', 'cynote_database'])
         backup_uploads_path = str(os.sep).join([os.getcwd(), 'applications', 
                                 request.application, 'uploads'])               
         for table in cynotedb.tables:
-            rows=cynotedb(cynotedb[table].id).select()
+            rows = cynotedb(cynotedb[table].id).select()
             open(str(os.sep).join([backup_path, table+'.csv']),
                 'w').write(str(cynotedb(cynotedb[table].id).select()))    
             table_list.append(table)
@@ -36,28 +38,29 @@ def form():
                                             bpath=backup_path,
                                             blist=backup_list,
                                             buploads=backup_uploads_path)
-        redirect(URL(r=request,f="backup_result"))
+        redirect(URL(r=request, f="backup_result"))
     return dict(form=form) 
     
 def backup_result():
-    result={}
-    result["Tables to backup"]=session.pop("table_list")
-    result["Backup files"]=session.pop("table_backup")
-    result["FTP backup"]=session.pop("ftp", None)
+    result = {}
+    result["Tables to backup"] = session.pop("table_list")
+    result["Backup files"] = session.pop("table_backup")
+    result["FTP backup"] = session.pop("ftp", None)
     print result
     return dict(result=result)
     
 def backup_ftp(server, username, password, bpath, blist, buploads):
-    if session.username == None: redirect(URL(r=request,f='../account/log_in'))
+    if session.username == None:
+        redirect(URL(r=request, f='../account/log_in'))
     result = []
     try: 
         s = FTP(server)
-        result.append(' '.join(['FTP server,', server,', resolved.']))
+        result.append(' '.join(['FTP server,', server, ', resolved.']))
     except: result.append('FTP host cannot be resolved')
     try:
         s.login(username, password)
-        result.append('Login as '+username+' successful')
-    except: result.append('Login as '+username+' unsuccessful')
+        result.append('Login as ' + username + ' successful')
+    except: result.append('Login as ' + username + ' unsuccessful')
     try:
         s.cwd('cynote_database')
         result.append('Changed to cynote_database directory')
@@ -73,12 +76,15 @@ def backup_ftp(server, username, password, bpath, blist, buploads):
         try:
             f = open(bpath + os.sep + bfile, 'rb')
             s.storbinary('STOR ' + bfile, f)
-            result.append('Upload file: '+bfile+' successful')
-        except: result.append('Upload file: '+bfile+' unsuccessful')
+            result.append('Upload file: ' + bfile + ' successful')
+        except: result.append('Upload file: ' + bfile + ' unsuccessful')
         try:
-            s.rename(bfile,str(now)+' '+bfile)
-            result.append('Renamed '+ bfile +' to '+str(now)+' '+bfile+ ' successful')
-        except: result.append('Renamed '+ bfile +' to '+str(now)+' '+bfile+ ' unsuccessful')
+            s.rename(bfile, str(now) + ' ' + bfile)
+            result.append('Renamed ' + bfile + ' to ' + str(now) + ' ' + \
+                          bfile + ' successful')
+        except:
+            result.append('Renamed ' +  bfile + ' to ' + str(now)+ ' ' + \
+                          bfile + ' unsuccessful')
     s.mkd(str(now) + ' uploads')
     result.append(str(now) + ' uploads' + ' directory created')
     s.cwd(str(now) + ' uploads')
@@ -87,11 +93,11 @@ def backup_ftp(server, username, password, bpath, blist, buploads):
         try:
             f = open(buploads + os.sep + upload_file, 'rb')
             s.storbinary('STOR ' + upload_file, f)
-            result.append('Upload file: '+upload_file+' successful')
-        except: result.append('Upload file: '+upload_file+' unsuccessful')
+            result.append('Upload file: ' + upload_file + ' successful')
+        except: result.append('Upload file: ' + upload_file + ' unsuccessful')
     try:
         s.quit()
         result.append('Logout from FTP server')
     except: result.append('FTP terminated withour logging out')
-    session['ftp']=result
+    session['ftp'] = result
     return
