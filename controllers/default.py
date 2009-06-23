611eecce497c85b95b9d541ed95764e1603311cc
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2009-06-23 16:16:58 +0000

    starting tabbed menu
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@56 796f4f98-6b54-0410-9c67-d7c569393209

--- controllers/default.py
+++ controllers/default.py
@@ -1,7 +1,18 @@
+tabs = [{'module': 'default', 'function': 'bioinformatics', 'name': 'Bioinformatics Tools'},
+]
+
 def index():
     response.flash=T('Welcome to CyNote - A web-enabled notebook compliant with general research record-keeping standard')
     if session.username == None: 
         name = 'Guest'
         redirect(URL(r=request,f='../account/log_in'))
     else: name = session.username
-    return dict(name=name, message=T('CyNote Menu'))
+    return dict(tab_list = tabs, name=name, message=T('CyNote Main Menu'))
+    
+def bioinformatics():
+    response.flash=T('Welcome to CyNote - A web-enabled notebook compliant with general research record-keeping standard')
+    if session.username == None: 
+        name = 'Guest'
+        redirect(URL(r=request,f='../account/log_in'))
+    else: name = session.username
+    return dict(tab_list = tabs, name=name, message=T('CyNote - Bioinformatics Menu'))
