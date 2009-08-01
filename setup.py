8f55d66f8dfcc4f6e865bd284aa342f135cc070b
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2009-08-01 06:31:39 +0000

    updated for cynote 1.0.1
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@80 796f4f98-6b54-0410-9c67-d7c569393209

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name='cynote',
-      version='1.0',
+      version='1.0.1',
       description='Cyber Laboratory Notebook',
-      long_description='Laboratory notebook using version control system for backups and independent date-time stamping (which may be a form of notarization), in order to ensure record accountability and auditing.',
+      long_description='Laboratory notebook using version control system for \
+backups and independent date-time stamping (which may be a form of \
+notarization), in order to ensure record accountability and auditing.',
       author='Maurice HT Ling',
       author_email='mauriceling@acm.org',
       url='http://cynote.sourceforge.net',
-      license = 'GNU General Public License version 2',
+      download_url='http://sourceforge.net/projects/cynote/files/CyNote/cynote1_0_1.zip/download',
+      license = 'GNU General Public License version 3',
       platform = 'OS independent',
       package_dir = {},
       packages = [],
