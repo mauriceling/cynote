cf6256060775093514bbfdd09c3191a85105b6c1
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2009-08-07 15:01:07 +0000

    added authorizing/deauthorizing users
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@86 796f4f98-6b54-0410-9c67-d7c569393209

--- controllers/account.py
+++ controllers/account.py
@@ -1,7 +1,9 @@
 #still on testing..
 
 def new_account():
-    # creating a new user account
+    """
+    Creating a new user account
+    """
     if userdb(userdb.user.username > 0).count() == 0:
         userdb.user.authorized.default = True
     else:
@@ -12,10 +14,12 @@ def new_account():
     return dict(form=form)    
     
 def log_in():
-    # function for user to log in
-    # compares the user login and password with userdb.user table
-    # if login is successful, the username is stored in session.username
-    # for further use. If login is not successful, session.username = None
+    """
+    Function for user to log in
+    Compares the user login and password with userdb.user table
+    If login is successful, the username is stored in session.username
+    for further use. If login is not successful, session.username = None
+    """
     form = FORM(TABLE(
                     TR("Username:",INPUT(_name="username",
                                          requires=IS_NOT_EMPTY())),
@@ -34,10 +38,56 @@ def log_in():
     return dict(form=form)
 
 def logged():
-    # redirection when login is successful
+    """edirection when login is successful"""
     return dict(name=session.username)
 
 def log_out():
-    # set session.username to None when logged out
+    """Set session.username to None when logged out"""
     session.username = None
     return dict(name=session.username)
+
+def auth_new_user():
+    """
+    Function to authorize newly signed-up users.
+    """
+    if session.username == None:
+        redirect(URL(r=request, f='log_in'))          
+    form = FORM(
+            TABLE(*[TR(""+str(id['username']), 
+                INPUT(_type="checkbox", _name=str(id['username']),
+                      value=False, _value='on'))
+            for id in userdb(userdb.user.authorized == "False") \
+                    .select(userdb.user.username)] + \
+            [TR("",INPUT(_type="submit", _value="Authorize User"))])) 
+    if form.accepts(request.vars,session):
+        option_checked = [id['name']
+                          for id['name'] in form.vars.keys()
+                          if form.vars[id['name']]]
+        for user in option_checked:
+            db.log.insert(event='User authorized. User = ' + user, 
+                          user=session.username)
+            userdb(userdb.user.username == user).update(authorized=True)
+    return dict(form=form)
+    
+def deauth_user():
+    """
+    Function to de-authorize users.
+    """
+    if session.username == None:
+        redirect(URL(r=request, f='log_in'))          
+    form = FORM(
+            TABLE(*[TR(""+str(id['username']), 
+                INPUT(_type="checkbox", _name=str(id['username']),
+                      value=False, _value='on'))
+            for id in userdb(userdb.user.authorized == "True") \
+                    .select(userdb.user.username)] + \
+            [TR("",INPUT(_type="submit", _value="De-authorize User"))])) 
+    if form.accepts(request.vars,session):
+        option_checked = [id['name']
+                          for id['name'] in form.vars.keys()
+                          if form.vars[id['name']]]
+        for user in option_checked:
+            db.log.insert(event='User de-authorized. User = ' + user, 
+                          user=session.username)
+            userdb(userdb.user.username == user).update(authorized=False)
+    return dict(form=form)
