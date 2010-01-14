dd84ce24a505d358c4f3f2567f088c40849a4230
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2010-01-14 16:32:48 +0000

    added password aging
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@167 796f4f98-6b54-0410-9c67-d7c569393209

--- models/userdb.py
+++ models/userdb.py
@@ -5,6 +5,7 @@ userdb=SQLDB("sqlite://userdb.db")
 userdb.define_table('user',
                 SQLField('username', 'string', unique=True),
                 SQLField('password', 'string'),
+                SQLField('aging', 'integer'),
                 SQLField('authorized', 'boolean'))
                 
 userdb.user.username.requires=IS_NOT_EMPTY()
