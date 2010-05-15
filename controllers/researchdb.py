a50d7250e6e743088806b676136247c334ccf6db
mauriceling <mauriceling@796f4f98-6b54-0410-9c67-d7c569393209>
2010-05-15 08:21:27 +0000

    ad hoc table definition
    git-svn-id: https://cynote.svn.sourceforge.net/svnroot/cynote/cynote@198 796f4f98-6b54-0410-9c67-d7c569393209

--- controllers/researchdb.py
+++ controllers/researchdb.py
@@ -1,12 +1,39 @@
+import os
+
 def create_table():
+    if session.username == None:
+        redirect(URL(r=request, f='../account/log_in'))
     form = FORM(TABLE(
             TR('Table Name: ', INPUT(_type='text', _name='name', _size=80)),
-            TR('Table Definition: ', TEXTAREA(_type='text', _name='definition')),
+            TR('Column Definition: ', TEXTAREA(_type='text', _name='definition')),
+            TR('Description: ', TEXTAREA(_type='text', _name='description')),
             TR('', INPUT(_type='submit', _name='Submit'))))
     if form.accepts(request.vars, session):
-        pass
+        fields = [x.strip() 
+                  for x in form.vars.definition.split(',')
+                    if x.strip() != '']
+        field_template = "SQLField('%s')"
+        col_def = [field_template % x for x in fields]
+        col_def.append("SQLField('datetime', 'datetime', default=now)")
+        col_def = ', '.join(col_def)
+        table_def = "researchdb.define_table('%s', " % form.vars.name
+        table_def = table_def + col_def + ')'
+        rdb_file = os.sep.join([os.getcwd(), 'applications', 
+                                request.application, 'models',
+                                'researchdb.py'])
+        f = open(rdb_file, 'a')
+        f.write('\n' + table_def + '\n')
+        f.close()
+        researchdb.control.insert(tablename=form.vars.name,
+                                  columndef=col_def,
+                                  creator=session.username,
+                                  description=form.vars.description)
+        db.log.insert(event='New ad hoc table created. Definition = ' + 
+                      table_def, user=session.username)
+        redirect(URL(r=request, f='list_table'))
     return dict(form=form)
     
 def list_table():
-    pass
-    
+    records = researchdb(researchdb.control.archived==False) \
+              .select(orderby=researchdb.control.tablename)
+    return dict(tables=records)
--- models/researchdb.py
+++ models/researchdb.py
@@ -5,4 +5,9 @@ now=datetime.datetime.utcnow()
 researchdb=SQLDB('sqlite://researchdb.db')
 
 researchdb.define_table('control',
-                SQLField('tablename', 'text'))
+                SQLField('tablename', 'text'),
+                SQLField('columndef', 'text'),
+                SQLField('creator', 'text'),
+                SQLField('datetime', 'datetime', default=now),
+                SQLField('description', 'text'),
+                SQLField('archived', 'boolean', default=False))
